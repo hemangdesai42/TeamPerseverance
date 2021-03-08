02eafc862603ba9379ec3f6dd6da6302f32bc7ac
Dominic Patrick <dom.patrick92@gmail.com>
2021-03-08 17:00:54 -0500

    partial userValidators

--- routes/validation.js
+++ routes/validation.js
@@ -2,8 +2,16 @@ const { check } = require('express-validator');
 
 
 const userValidator = [
-//     check('name')
-//         .exists({ checkFalsy: true})
+    check('name')
+        .exists({ checkFalsy: true})
+        .withMessage('Please provide a value for Name')
+        .isLength({ max: 50})
+        .withMessage('Name must not be more than 50 characters long'),
+    check('userName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for User Name')
+        .isLength({ max: 50 })
+        .withMessage('User Name must not be more than 50 characters long'),
 ]
 
 
