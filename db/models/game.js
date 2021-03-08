88cdf249b641df3d3c59c1505391397139ded176
Hemang Desai <hmang42@icloud.com>
2021-03-08 15:54:55 -0500

    Finished Model Associations

--- db/models/game.js
+++ db/models/game.js
@@ -1,14 +1,32 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Game = sequelize.define('Game', {
-    name: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    releaseDate: DataTypes.DATE,
-    platform: DataTypes.STRING
+    name: {
+      allowNull: false,
+      type: Sequelize.STRING(200),
+      unique: true
+    },
+    genre: {
+      allowNull: false,
+      type: Sequelize.STRING(100)
+    },
+    description: {
+      allowNull: false,
+      type: Sequelize.TEXT
+    },
+    releaseDate: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    platform: {
+      allowNull: false,
+      type: Sequelize.STRING(100)
+    },
   }, {});
   Game.associate = function(models) {
-    // associations can be defined here
+    Game.hasMany(models.Review, { foreignkey: 'gameId' })
+    Game.hasMany(models.Rating, { foreignkey: 'gameId' })
+    Game.hasMany(models.GameShelf, { foreignkey: 'gameId' })
   };
   return Game;
 };
--- db/models/review.js
+++ db/models/review.js
@@ -1,12 +1,24 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Review = sequelize.define('Review', {
-    review: DataTypes.STRING,
-    userId: DataTypes.INTEGER,
-    gameId: DataTypes.INTEGER
+    review: {
+      type: Sequelize.TEXT,
+      allowNull: false
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: 'Users' }
+    },
+    gameId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: 'Games' }
+    },
   }, {});
   Review.associate = function(models) {
-    // associations can be defined here
+    Review.belongsTo(models.User, { foreignKey: 'userId'})
+    Review.belongsTo(models.Game, { foreignKey: 'gameId'})
   };
   return Review;
 };
