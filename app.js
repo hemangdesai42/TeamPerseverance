399bc16cc59d92bb1e8663ac7a8f13f108e13c3e
Dominic Patrick <dom.patrick92@gmail.com>
2021-03-08 19:32:16 -0500

    homepage form - not finished

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home')
 const { config } = require('./config')
 const app = express();
 
@@ -37,6 +38,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/home', homeRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
